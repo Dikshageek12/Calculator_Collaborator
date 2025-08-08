126355d2bcc1504576eb0ffff8a2dff2698e74ce
Charvi Mittal <charvimittal304@gmail.com>
2025-08-08 11:02:22 +0530

    added addition.java

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("addition: " + add(10, 5));
+    }
+}
