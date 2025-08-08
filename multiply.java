a001396bf0ba7c753eca49b9c004094fa4b978f2
Diksha <dikshasharma8795@gmail.com>
2025-08-08 11:16:53 +0530

    Multiplication code

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        int result = multiply(5, 7);
+        System.out.println("The product is: " + result);
+    }
+}
