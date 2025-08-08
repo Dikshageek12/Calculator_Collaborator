c7c9fb03699a9d0a9bcc7c724b56b0a5d171cb92
Riddhima <riddhimarajput2006@gmail.com>
2025-08-08 10:50:17 +0530

    Add Subtraction.java for subtraction functionality

---
+++ Subtraction.java
@@ -0,0 +1,9 @@
+public class Subtraction {
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Subtraction: " + subtract(10, 5));
+    }
+}
